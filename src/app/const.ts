8d865cd5581d801219a00462edfc0e9d62b9bd7e
zetsu01 <soviguidivenance@gmail.com>
2025-03-29 14:00:21 +0100

    🔧 first commit

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,5 +1,6 @@
 // tslint:disable-next-line: eofline
 // export const baseURL  = 'http://serveur:8080/aphedd-tontine/api';
-export const baseURL  = 'http://173.234.153.201:8080/aphedd-tontine/api';
+// export const baseURL  = 'http://173.234.153.201:8080/aphedd-tontine/api';
+export const baseURL  = 'http://69.197.145.42:8090/esaving-dev/api';
 export const itemsPerPage = 5;
 export const pageSizeOptions = [5, 10, 20, 50, 100];
